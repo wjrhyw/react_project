dc9c416a27f63b04148fc0aadafae01a09eceb7c
wjr <409400483@qq.com>
2019-08-30 11:06:05 +0800

    '目录'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+
+class App extends Component{
+  render(){
+    return (
+      <div style={{textAlign:'center'}}>
+        项目入口
+      </div>
+    )
+  }
+}
+
+export default App;
