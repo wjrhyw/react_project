26ae3b0dbe226e6d07ec79ab5e0f2dac85ab417d
wjr <409400483@qq.com>
2019-09-05 10:11:45 +0800

    wjr

--- src/components/TakeOut/TakeOutList.js
+++ src/components/TakeOut/TakeOutList.js
@@ -7,18 +7,14 @@ class TakeOutList extends React.Component{
     render(){
         return (
             <div id='TakeOutList' >
-
                 <p className='tuijian'>推荐商家­­­­­­­</p>
-
                 <ul id='flex_ul' className={this.props.scroll>643?'ul_flex_active':''}>
                     <li>综合排序</li>
                     <li>距离最近</li>
                     <li>品质联盟</li>
                     <li>筛选</li>
                 </ul>
-
                 <p style={this.props.scroll>643?{display:'block',height:'20px'}:{display:'none'}}></p>
-                
                 <ul className='ul_list'>
                     <li>测试</li>
                     <li>测试</li>
