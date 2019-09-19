c333094e075ff8819548c3177d1c0010ca295288
zzy <1903394077@qq.com>
2019-09-19 18:50:29 +0800

    zzy

--- src/components/MeFile/Download.js
+++ src/components/MeFile/Download.js
@@ -1,10 +1,24 @@
 //下载APP
 import React from 'react';
+import {NavBar,Icon,Button} from "antd-mobile";
 
 class Download extends React.Component{
     render(){
+        const item = ['md'];
+        let imgStyle ={
+            width:"100px", 
+            height:"100px",
+            marginTop:"25px",
+            marginBottom:"20px"
+        }
         return(
-            <div>我的订单</div>
+            <div>
+                <NavBar mode="dark" onLeftClick={()=>{console.log("back")}} icon={<Icon type="left" size={item}/>}>下载</NavBar>
+                <div>
+                    <img alt="img is wrong" style={imgStyle}  src={require('../../assets/img/favicon.ico')}/>
+                </div>
+                <Button type="primary" style={{backgroundColor:"#00cc00",margin:"5px 10px 0 10px",fontWeight:"bold"}}>下载APP</Button>
+            </div>
         )
     }
 }
