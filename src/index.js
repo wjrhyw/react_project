c8588182fb0abd6ae8a74001fa373daf4b8b3f7a
wjr <409400483@qq.com>
2019-09-19 15:20:15 +0800

    wjr

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'antd-mobile/dist/antd-mobile.css';
 import './assets/css/index.css';
-<<<<<<< HEAD
 import 'antd/dist/antd.css';
 import { createStore } from 'redux';
-=======
->>>>>>> 7956fe744bd7841cd73e2d737e2ffda66469b5a2
 import { Provider } from 'react-redux'
 import indexReducers from './store/reducers/index'
 import {createStore, applyMiddleware, compose} from 'redux'
